5c3138824ec7d5d263fb71dd9343fae447805fe3
Elm0 <60823012+lazyelm0@users.noreply.github.com>
2021-12-25 15:32:55 +0100

    styles updated

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Dr Nice'},
+    {id: 12, name: 'BadGuy'},
+    {id: 13, name: 'Magma'},
+    {id: 14, name: 'Heyya'},
+    {id: 15, name: 'Mephisto'}
+];
